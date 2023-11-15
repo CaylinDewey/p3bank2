582793ea5a5986c3577e194475bafc6e5f24c4fc
Caylin Dewey <caylindewey42@gmail.com>
2023-11-15 09:34:14 +0000

    Create title and main function

--- run.py
+++ run.py
@@ -21,5 +21,35 @@ depSheet = SHEET.worksheet("deposit")
 bankchrSheet = SHEET.worksheet("bankcharge")
 intSheet = SHEET.worksheet("interest")
 
-print(balSheet.get_all_records())
 
+
+#  def deposit(self, amount):
+#         self.balance = self.balance + amount
+#         print("\nDeposit complete.")
+#         self.get_balance()
+
+#  def deposit(self, amount):
+#         self.balance = self.balance + amount
+#         print("\nDeposit complete.")
+#         self.get_balance()
+# deposit()
+
+# def deposit_worksheet(data, worksheet):
+#     """
+#     Updates clients with deposited amounts
+#     """
+#     print(f"Updating {worksheet} worksheet...\n")
+#     worksheet_to_update = SHEET.worksheet(worksheet)
+#     worksheet_to_update.append_row(data)
+#     print(f"{worksheet} worksheet updated successfully\n")
+
+
+
+def main():
+    """
+    Run all program functions
+    """
+    print(balSheet.get_all_records())
+
+print("The Banking App 2\n")
+main()
