50bc8d31dbb6735648e02a07e18eed16dc5823cb
Caylin Dewey <caylindewey42@gmail.com>
2023-11-21 08:35:44 +0000

    Add check balance if account found

--- run.py
+++ run.py
@@ -1,56 +1,52 @@
 import gspread
 from google.oauth2.service_account import Credentials
-import math
+from datetime import datetime
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
     "https://www.googleapis.com/auth/drive"
-    ]
+]
 
 CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('p3bank2')
 
-""""
-Get the sheet names ready for use
-"""
-balSheet = SHEET.worksheet("balance")
-depSheet = SHEET.worksheet("deposit")
-bankchrSheet = SHEET.worksheet("bankcharge")
-intSheet = SHEET.worksheet("interest")
+def ask_account_name():
+    account_name = input ("Enter your account name: ")
+    return account_name.lower()
 
-def add_deposit():
-    """"
-    Provide opportunity for client to deposit and request correct input if validation fails
-    """
-    while True:
-        print("Please enter the amount you want to deposit - with no cents e.g. Euro: 200")
-        dep_str = input("Euro:  ")
-        
-        if validate_deposit(dep_str):
-            break
-
-def validate_deposit(values):
-    """
-    Converts all string values into integers and raises a ValueError if data is not valid.
-    """
+def check_balance(account_name):
     try:
-        [int(value) for value in values]
-        print(f"You are depositing {values} Euro.\n")
-         
-    except ValueError as e:
-        print(f"Invalid amount, please try again.\n")
-        return False
-    
-    return True
+        worksheet = SHEET.worksheet(account_name)
+        old_balance = calculate_balance(worksheet)# Function below this function
+        print(f"Your account has been found, the balance is {old_balance:.2f} Euros.")
+    except gspread.exceptions.WorksheetNotFound:
+        print(f"No account was found for {account_name}. Would you like to create a new account?")
 
-def main():
-    """
-    Run all program functions
-    """
-    # print(balSheet.get_all_records())
-    add_deposit()
-print("The Banking App 2\n")
-main()
+def calculate_balance(worksheet):# function in ask_account_name
+    values = worksheet.get_all_values()
+
+    if len(values) <2:
+      return 0.0
+
+    deposits = withdrawals = 0
+
+    for row in values[1:]:# [1]: iterates over second row only, now all rows including header
+        _, transaction_type, amount_str, _ = row
+        amount = float(amount_str)
+
+        if transaction_type.lower().strip() == 'deposit':
+            deposits += amount
+        elif transaction_type.lower().strip() == 'withdrawal':
+            withdrawals += amount
+    return deposits - withdrawals
+
+
+account_name = ask_account_name()
+check_balance(account_name)
+
+
+
+
